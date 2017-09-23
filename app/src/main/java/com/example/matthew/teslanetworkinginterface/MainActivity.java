bc1ef87090fd104332e0eff6b5f94b073c4f8b7e
Matthew Wolff <mm.wolff@chi-squared.org>
2017-09-23 04:22:47 -0400

    initial commit

---
+++ app/src/main/java/com/example/matthew/teslanetworkinginterface/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.matthew.teslanetworkinginterface;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.loopj.android.http.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("https://www.google.com", new AsyncHttpResponseHandler() {
+
+            @Override
+            public void onStart() {
+                // called before request is started
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                // called when response HTTP status is "200 OK"
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+            }
+
+            @Override
+            public void onRetry(int retryNo) {
+                // called when request is retried
+            }
+        });
+    }
+}
