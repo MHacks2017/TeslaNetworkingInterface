558f286b46726ed8aed37ed6fbf3abf6a9a9cd38
Matthew Wolff <mm.wolff@chi-squared.org>
2017-09-23 10:48:47 -0400

    successfully able to retrieve JSON object

--- app/src/main/java/com/example/matthew/teslanetworkinginterface/MainActivity.java
+++ app/src/main/java/com/example/matthew/teslanetworkinginterface/MainActivity.java
@@ -1,8 +1,20 @@
 package com.example.matthew.teslanetworkinginterface;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.widget.TextView;
+
 import com.loopj.android.http.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,28 +23,90 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get("https://www.google.com", new AsyncHttpResponseHandler() {
-
+        AsyncTask.execute(new Runnable() {
             @Override
-            public void onStart() {
-                // called before request is started
-            }
+            public void run() {
+                // All your networking logic
+                // should be here
+                // Create URL
+                URL githubEndpoint = null;
+                try {
+                    githubEndpoint = new URL("https://api.github.com/");
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
 
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
-                // called when response HTTP status is "200 OK"
-            }
+                // Create connection
+                HttpsURLConnection myConnection = null;
+                try {
+                    myConnection = (HttpsURLConnection) githubEndpoint.openConnection();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                myConnection.setRequestProperty("User-Agent", "my-rest-app-v0.1");
+                myConnection.setRequestProperty("Accept", "application/vnd.github.v3+json");
+                myConnection.setRequestProperty("Contact-Me", "hathibelagal@example.com");
 
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
-                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-            }
+                try {
+                    if (myConnection.getResponseCode() == 200) {
+                        // Success
+                        InputStream responseBody = myConnection.getInputStream();
+                        InputStreamReader responseBodyReader = new InputStreamReader(responseBody, "UTF-8");
+                        JsonReader jsonReader = new JsonReader(responseBodyReader);
+                        jsonReader.beginObject();
+                        while (jsonReader.hasNext()) { // Loop through all keys
+                            String key = jsonReader.nextName(); // Fetch the next key
+                            if (key.equals("organization_url")) { // Check if desired key
+                                // Fetch the value as a String
+                                String value = jsonReader.nextString();
 
-            @Override
-            public void onRetry(int retryNo) {
-                // called when request is retried
+                                TextView newtext = (TextView) findViewById(R.id.my_output);
+                                newtext.setText(value);
+                                // Do something with the value
+                                // ...
+
+                                break; // Break out of the loop
+                            } else {
+                                jsonReader.skipValue(); // Skip values of other keys
+                            }
+                        }
+                        jsonReader.close();
+                        myConnection.disconnect();
+
+                        // Further processing here
+                    } else {
+                        // Error handling code goes here
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
+
+
+//
+//        AsyncHttpClient client = new AsyncHttpClient();
+//        client.get("https://www.google.com", new AsyncHttpResponseHandler() {
+//
+//            @Override
+//            public void onStart() {
+//                // called before request is started
+//            }
+//
+//            @Override
+//            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+//                // called when response HTTP status is "200 OK"
+//            }
+//
+//            @Override
+//            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+//                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+//            }
+//
+//            @Override
+//            public void onRetry(int retryNo) {
+//                // called when request is retried
+//            }
+//        });
     }
 }
